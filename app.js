9222ca601081833319c3ebb8444a728080b59a37
Rachel Kinzel <rharen11@su.edu>
2022-06-06 10:11:05 -0700

    add projects array

---
+++ app.js
@@ -0,0 +1,9 @@
+let projects = [
+    {
+        'Title': 'Tic Tac To',
+        Image: '',
+        'Description': 'Classic Tic Tac To game',
+        'GitHub link': '',
+        'Deployment Link': ''
+    }
+]
