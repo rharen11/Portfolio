e8c332ab515bae225c2906a2d75fe25ef69cdf89
Rachel Kinzel <rharen11@su.edu>
2022-06-08 15:47:04 -0700

    switch js files

--- js/app.js
+++ js/app.js
@@ -1,13 +1,34 @@
-let projects = [
-    {
-        title: 'Tic Tac To',
-        image: './assets/ttt-pic.png',
-        description: 'Classic Tic Tac To game',
-        github: 'https://github.com/rharen11/ttt-weekend',
-        deployment: 'https://ttt-weekend-rachelkinzel.netlify.app/'
-    }
-]
 
-export {
-    projects
-}
+//variables----------
+import { projects } from "./data.js"
+
+//cached elements---------
+const cardContainer = document.getElementById('card-container')
+const navBtns = document.querySelector('.nav-bar')
+const contactBtn = document.getElementById('contact')
+
+//event listeners-------------
+
+contactBtn.addEventListener('mouseover', function(){
+    console.log('it works')
+})
+
+//functions------------------------
+
+let projectPage = projects.map(project =>
+    
+`<div class="card text-center" style="width: 18rem;">
+<img src="${project.image}" class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">${project.title}</h5>
+  <p class="card-text">${project.description}</p>
+  <div>
+    <a href="${project.github}" class="btn btn-primary">GitHub</a>
+    <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+  </div>
+</div>
+</div>`
+
+).join('')
+
+cardContainer.innerHTML = projectPage
--- js/data.js
+++ js/data.js
@@ -1,36 +1,14 @@
 
-//variables----------
-import { projects } from "./app.js"
+let projects = [
+    {
+        title: 'Tic Tac To',
+        image: './assets/ttt-pic.png',
+        description: 'Classic Tic Tac To game',
+        github: 'https://github.com/rharen11/ttt-weekend',
+        deployment: 'https://ttt-weekend-rachelkinzel.netlify.app/'
+    }
+]
 
-//cached elements---------
-const cardContainer = document.getElementById('card-container')
-const navBtns = document.querySelector('.nav-bar')
-
-//event listeners-------------
-// navBtns.addEventListener("mouseover", function(evt){
-//     evt.target.style.color = "yellow"
-
-//     setTimeout(function(){
-//         evt.target.style.color = ""
-//     }, 1000)
-// }, false)
-
-//functions------------------------
-
-let projectPage = projects.map(project =>
-    
-`<div class="card text-center" style="width: 18rem;">
-<img src="${project.image}" class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">${project.title}</h5>
-  <p class="card-text">${project.description}</p>
-  <div>
-    <a href="${project.github}" class="btn btn-primary">GitHub</a>
-    <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
-  </div>
-</div>
-</div>`
-
-).join('')
-
-cardContainer.innerHTML = projectPage
+export {
+    projects
+}
