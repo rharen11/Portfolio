ad5c379542f8eb1cae84f0c9171af855f07bb81d
Rachel Kinzel <rharen11@su.edu>
2022-06-08 05:57:14 -0700

    add ttt picture

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 let projects = [
     {
         'Title': 'Tic Tac To',
-        Image: '',
+        Image: '/assets/ttt-pic.png',
         'Description': 'Classic Tic Tac To game',
         'GitHub link': 'https://github.com/rharen11/ttt-weekend',
         'Deployment Link': 'https://ttt-weekend-rachelkinzel.netlify.app/'
