eccf246b9ebed7adea4d8ee7a2a01d78c4f33c42
Rachel Kinzel <rharen11@su.edu>
2022-06-28 10:35:05 -0700

    add sonoma hike project

--- js/data.js
+++ js/data.js
@@ -6,6 +6,13 @@ let projects = [
         description: 'Classic Tic Tac To game',
         github: 'https://github.com/rharen11/ttt-weekend',
         deployment: 'https://ttt-weekend-rachelkinzel.netlify.app/'
+    },
+    {
+        title: 'Sonoma Hikes',
+        image: ,
+        description: 'Explore Hood Mountain with an interactive app',
+        github: 'https://github.com/rharen11/sonoma-hikes',
+        deployment: 'https://sonoma-hikes.herokuapp.com/'
     }
 ]
 
