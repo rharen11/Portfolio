1a9138b46df55b2cd5964b1a9642508b561c7526
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-08-29 17:06:50 -0700

    update desktop styling works

--- js/data.js
+++ js/data.js
@@ -1,33 +1,34 @@
 
 let projects = [
     {
-        title: 'Tic Tac To',
-        image: './assets/ttt-pic.png',
-        description: 'Classic Tic Tac To game',
-        github: 'https://github.com/rharen11/ttt-weekend',
-        deployment: 'https://ttt-weekend-rachelkinzel.netlify.app/'
+        title: 'Swept Away',
+        image: './assets/swept-away.png',
+        description: 'App built with Python and Django to organize your cleaning tasks- clean for the moment- enjoy for life!',
+        github: 'https://github.com/rharen11/swept-away',
+        deployment: 'https://swept-away-rlk.herokuapp.com/'
     },
+    
     {
         title: 'Sonoma Hikes',
         image: './assets/sonoma-hiking-pic-2.png',
-        description: 'Explore Hood Mountain with an interactive app',
+        description: 'MEN Stack app made for the hiker in everyone. Explore beautiful Hood Mountain in Sonoma, County, CA!',
         github: 'https://github.com/rharen11/sonoma-hikes',
         deployment: 'https://sonoma-hikes.herokuapp.com/'
     },
     {
         title: 'Newt',
         image: './assets/newtmain.png',
-        description: 'Become an object in motion!',
+        description: 'MERN Stack app built with the Bored API. Search for new activities and skills- become an object in motion!',
         github: 'https://github.com/berylrb/newt-front-end',
         deployment: 'https://newt-activity.netlify.app/'
     },
     {
-        title: 'Swept Away',
-        image: './assets/swept-away.png',
-        description: 'Clean for the moment, enjoy for life',
-        github: 'https://github.com/rharen11/swept-away',
-        deployment: 'https://swept-away-rlk.herokuapp.com/'
-    }
+        title: 'Tic Tac To',
+        image: './assets/ttt-pic.png',
+        description: 'App built with Javascript, HTML, and CSS. Try your hand at beating the computer!',
+        github: 'https://github.com/rharen11/ttt-weekend',
+        deployment: 'https://ttt-weekend-rachelkinzel.netlify.app/'
+    },
 ]
 
 export {
